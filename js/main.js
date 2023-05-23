5b8ac68a2185e7ca4322d80c4cce0b71e64d6692
Aitor <aitor.san.rodriguez@gmail.com>
2023-05-23 22:21:51 +0200

    Cambio menu responsive, falta menu desplegable

--- js/main.js
+++ js/main.js
@@ -8,10 +8,15 @@ const ranking = componenteLi('#', 'img/trofeo.png', 'Ranking');
 const calendario = componenteLi('#', 'img/calendario-reloj.png', 'Calendario');
 const parejas = componenteLi('#', 'img/bola.png', 'Parejas');
 const datosMenuPC = [portada, fotos, ranking, calendario, parejas];
+const anchoPantalla = window.matchMedia("(max-width: 1400px)");
+
+mediaQuery(anchoPantalla);
+anchoPantalla.addListener(mediaQuery);
 
 document.addEventListener('DOMContentLoaded', function () {
     const URL = 'json/parejas.json';
     let docJsonParejas;
+
     jornada.innerText = 'Cuarta jornada';
     spinner.appendChild(componenteSpinner('loadingClasificacion'));
 
@@ -30,12 +35,24 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById('ganados' + i).innerText = docJsonParejas[i].partidosGanados;
                 document.getElementById('puntos' + i).innerText = docJsonParejas[i].puntos;
             }
+
             eliminarComponente('loadingClasificacion');
             tablaClasificacion.style.display = 'block';
             tablaClasificacion.style.margin = 'auto';
             tablaClasificacion.style.width = 'fit-content';
-
         })
         .catch(err => console.log(err));
 });
+
+function mediaQuery(anchoPantalla) {
+    const titulo = document.querySelector('#titulo');
+
+    if (anchoPantalla.matches) {
+        titulo.className = 'col col-12'
+
+    } else {
+        titulo.className = 'col col-5'
+    }
+}
+
 
