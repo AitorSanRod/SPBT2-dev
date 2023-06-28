caa2b6972e49d445a1583373457a3670d2838090
Aitor <aitor.san.rodriguez@gmail.com>
2023-06-28 20:06:59 +0200

    calendario finalizado

--- js/calendario.js
+++ js/calendario.js
@@ -2,66 +2,104 @@ const URL = '../json/calendario.json';
 const contenedorCalendario = document.querySelector('#calendario');
 
 fetch(URL)
-    .then(response => response.json())
-    .then(repos => {
-        jsonCalendario = repos.map(docJson => docJson);
-        contenedorCalendario.appendChild(componenteAcordeonCalendario('acordeonCalendario', jsonCalendario));
-    })
-    .catch(err => console.log(err));
+  .then(response => response.json())
+  .then(repos => {
+    jsonCalendario = repos.map(docJson => docJson);
+    contenedorCalendario.appendChild(componenteAcordeonCalendario('acordeonCalendario', jsonCalendario));
+  })
+  .catch(err => console.log(err));
 
 function componenteAcordeonCalendario(id, calendario) {
-    let acordeon;
-    let itemAcordeon;
-    let encabezadoItem;
-    let itemBoton;
-    let contenidoDelAcordeon;
-    let cuerpoAcordeon;
-    let componente = document.createElement('div');
+  let acordeon;
+  let itemAcordeon;
+  let encabezadoItem;
+  let itemBoton;
+  let contenidoDelAcordeon;
+  let cuerpoAcordeon;
+  let datosPartido;
+  let componente = document.createElement('div');
 
-    acordeon = document.createElement('div');
-    acordeon.className = 'accordion';
-    acordeon.id = id;
+  acordeon = document.createElement('div');
+  acordeon.className = 'accordion';
+  acordeon.id = id;
 
-    for (let i = 0; i < calendario.length; i++) {
-        itemAcordeon = document.createElement('div');
-        encabezadoItem = document.createElement('h2');
-        itemBoton = document.createElement('button');
-        contenidoDelAcordeon = document.createElement('div');
-        cuerpoAcordeon = document.createElement('div');
+  for (let i = 0; i < calendario.length; i++) {
+    itemAcordeon = document.createElement('div');
+    encabezadoItem = document.createElement('h2');
+    itemBoton = document.createElement('button');
+    contenidoDelAcordeon = document.createElement('div');
+    cuerpoAcordeon = document.createElement('div');
 
-        // Encabezado y boton
-        itemAcordeon.className = 'accordion-item';
-        encabezadoItem.className = 'accordion-header';
-        itemBoton.className = 'accordion-button';
-        itemBoton.type = 'button';
-        itemBoton.setAttribute('data-bs-toggle', 'collapse');
-        itemBoton.setAttribute('data-bs-target', `#${calendario[i].jornada}`);
-        itemBoton.setAttribute('aria-expanded', 'true');
-        itemBoton.setAttribute('aria-controls', `collapse${i}`);
-        itemBoton.innerHTML = `Jornada: ${calendario[i].jornada}`;
+    // Encabezado y boton
+    itemAcordeon.className = 'accordion-item';
+    encabezadoItem.className = 'accordion-header';
+    itemBoton.className = 'accordion-button';
+    itemBoton.type = 'button';
+    itemBoton.setAttribute('data-bs-toggle', 'collapse');
+    itemBoton.setAttribute('data-bs-target', `#${calendario[i].jornada}`);
+    itemBoton.setAttribute('aria-expanded', 'true');
+    itemBoton.setAttribute('aria-controls', `collapse${i}`);
+    itemBoton.innerHTML = `Jornada: ${calendario[i].jornada} - Límite: ${calendario[i].fecha}`;
 
-        // Contenido
-        contenidoDelAcordeon.id = `${calendario[i].jornada}`;
-        contenidoDelAcordeon.className = 'accordion-collapse collapse';
-        contenidoDelAcordeon.setAttribute('data-bs-parent', `#${id}`);
-        cuerpoAcordeon.className = 'accordion-body';
+    // Contenido
+    contenidoDelAcordeon.id = `${calendario[i].jornada}`;
+    contenidoDelAcordeon.className = 'accordion-collapse collapse';
+    contenidoDelAcordeon.setAttribute('data-bs-parent', `#${id}`);
+    cuerpoAcordeon.className = 'accordion-body';
 
-        calendario[i].partidos.forEach(partido => {
-            cuerpoAcordeon.innerHTML += `
-                    <h5 class='text-center'>${partido.parejas}</h5>
-                    <h6 class='text-center'>${partido.estado}</h6>
-                    <hr>
-                `;
-        });
+    for (let j = 0; j < calendario[i].partidos.length; j++) {
+      if (j < calendario[i].partidos.length - 1) {
+        if (calendario[i].partidos[j].estado === 'Jugado') {
+          datosPartido = `
+            <h5 class='text-center'>${calendario[i].partidos[j].parejas}</h5>
+            <h6 class='text-center text-success'>${calendario[i].partidos[j].estado}</h6>
+            <hr>
+          `
+        } else {
+          datosPartido = `
+            <h5 class='text-center'>${calendario[i].partidos[j].parejas}</h5>
+            <h6 class='text-center text-danger'>${calendario[i].partidos[j].estado}</h6>
+            <hr>
+          `
+        }
 
-        contenidoDelAcordeon.appendChild(cuerpoAcordeon);
-        itemAcordeon.appendChild(encabezadoItem);
-        itemAcordeon.appendChild(itemBoton);
-        itemAcordeon.appendChild(contenidoDelAcordeon);
-        acordeon.appendChild(itemAcordeon);
+      } else {
+        if (calendario[i].partidos[j].estado === 'Jugado') {
+          datosPartido = `
+            <h5 class='text-center'>${calendario[i].partidos[j].parejas}</h5>
+            <h6 class='text-center text-success'>${calendario[i].partidos[j].estado}</h6>
+          `
+        } else if (calendario[i].partidos[j].estado === 'Descanso') {
+          if (calendario[i].partidos[j].estado === 'Jugado') {
+            datosPartido = `
+              <h5 class='text-center'>${calendario[i].partidos[j].parejas}</h5>
+              <h6 class='text-center text-info'>${calendario[i].partidos[j].estado}</h6>
+            `
+          } else {
+            datosPartido = `
+              <h5 class='text-center'>${calendario[i].partidos[j].parejas}</h5>
+              <h6 class='text-center text-info'>${calendario[i].partidos[j].estado}</h6>
+            `
+          }
+        } else {
+          datosPartido = `
+            <h5 class='text-center'>${calendario[i].partidos[j].parejas}</h5>
+            <h6 class='text-center text-danger'>${calendario[i].partidos[j].estado}</h6>
+          `
+        }
+      }
+
+      cuerpoAcordeon.innerHTML += datosPartido;
     }
 
-    componente.appendChild(acordeon);
+    contenidoDelAcordeon.appendChild(cuerpoAcordeon);
+    itemAcordeon.appendChild(encabezadoItem);
+    itemAcordeon.appendChild(itemBoton);
+    itemAcordeon.appendChild(contenidoDelAcordeon);
+    acordeon.appendChild(itemAcordeon);
+  }
 
-    return componente;
+  componente.appendChild(acordeon);
+
+  return componente;
 }
